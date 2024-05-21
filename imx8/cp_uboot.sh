80b4df09ac1a38f50b3b0f45f9d36c8c30732642
kuihong.xin <kuihong.xin@advantech.com.cn>
2024-05-21 14:42:37 +0800

    [yocto4.2] fix mkimage fail for imx8mm

--- imx8/cp_uboot.sh
+++ imx8/cp_uboot.sh
@@ -7,6 +7,10 @@ case $SOC in
 		cp $UBOOT_PATH/u-boot-nodtb.bin iMX8M/u-boot-nodtb.bin
 		cp $UBOOT_PATH/u-boot.dtb iMX8M/fsl-imx8mm-evk.dtb
 		cp $UBOOT_PATH/spl/u-boot-spl.bin iMX8M/u-boot-spl.bin
+
+		[ -e iMX8M/imx8mm-evk.dtb ] && rm iMX8M/imx8mm-evk.dtb
+		cp $UBOOT_PATH/tools/mkimage iMX8M/mkimage_uboot
+		rm -rf iMX8M/mkimage_imx8
 	;;
 	"imx8m")
 		echo "Copy files to [iMX8M]"
